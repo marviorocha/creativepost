2dd0aeb6b44f5aa0bf4269127ccaa18d4aaf104f
Marvio Rocha <marviorocha@marviorocha.com>
2019-08-03 20:44:46 -0300

    add device

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,30 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-<script type="text/javascript">
-    $(function() {
-        var editor = editormd("editor", {
-            // width: "100%",
-            // height: "100%",
-            // markdown: "xxxx",     // dynamic set Markdown text
-            path : "editor.md/lib/"  // Autoload modules mode, codemirror, marked... dependents libs path
+ 
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
         });
-    });
-</script>
+      });
+    }
+  
+  });
+
